2dab8616c0131d79a239ac278b24b36d8c3b463e
vismarlora <vismar_lora@ucne.edu.do>
2022-05-30 16:44:53 -0400

    Agregando la Lista de nombres

--- app/src/main/java/com/example/tarea3compose/MainActivity.kt
+++ app/src/main/java/com/example/tarea3compose/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.tarea3compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -32,12 +37,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+    Column() {
+
     Button(onClick = { /*TODO*/ }) {
     Text("Nuevo")
     }
+        val lista = listOf("Vismar", "Nachely", "Kelvin")
+
+        LazyColumn(modifier = Modifier.fillMaxWidth()){
+            items(lista) { nombre ->
+                RowNombre(nombre)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun RowNombre(nombre: String){
+    Row(){
+        Text("El nombre es: $nombre")
+
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun DefaultPreview() {
     Tarea3ComposeTheme {
