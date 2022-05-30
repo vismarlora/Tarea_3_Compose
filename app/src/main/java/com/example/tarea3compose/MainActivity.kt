9ed4e32dbbb9678dd942fef4001dfa2e9cbf5a15
vismarlora <vismar_lora@ucne.edu.do>
2022-05-30 16:50:33 -0400

    Separando el boton y los nombres de los lados

--- app/src/main/java/com/example/tarea3compose/MainActivity.kt
+++ app/src/main/java/com/example/tarea3compose/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.tarea3compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -16,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tarea3compose.ui.theme.Tarea3ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    Column() {
+    Column(modifier = Modifier.padding(8.dp)) {
 
     Button(onClick = { /*TODO*/ }) {
     Text("Nuevo")
@@ -61,7 +59,7 @@ fun RowNombre(nombre: String){
     }
 }
 
-@Preview(showBackground = true, showSystemUi = false)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     Tarea3ComposeTheme {
